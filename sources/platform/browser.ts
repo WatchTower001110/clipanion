b89398c27d6eade129a883f024ed357803cac907
WatchTower001110 <138610856+WatchTower001110@users.noreply.github.com>
2023-11-01 18:37:41 -0500

    Update browser.ios

--- sources/platform/browser.ts
+++ sources/platform/browser.ts
@@ -1,7 +1,3 @@
 export function getDefaultColorDepth() {
-  return 1;
-}
-
-export function getCaptureActivator() {
-  throw new Error(`The enableCapture option cannot be used from within a browser environment`);
+  return 1;https://github.com/WatchTower001110/clipanion/actions
 }
